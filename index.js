d0c20d10a10f3ef8969d58efd71e1a6c4217656d
GalenReed <galenreed@vt.edu>
2024-09-09 20:48:00 -0400

    this was challenging

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+function submitData(name, email) {
+    const data = {
+        name: `${name}`,
+        email: `${email}`,
+    };
+
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+    body: JSON.stringify(data)
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            const newId = data.id;
+            const body = document.querySelector('body');
+            body.innerHTML += `<p>New ID: ${newId}</p>`;
+            return newId;
+        })
+        .catch(function (error) {
+            const body = document.querySelector('body');
+            body.innerHTML += `<p>Error: ${error.message}</p>`;
+        });
+}
